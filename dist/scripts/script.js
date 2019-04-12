a2632ffa4baa6a3bb3a11a4c3109d90554fcdb00
Trista Huang <trista.huang@outlook.com>
2019-04-12 15:27:47 -0700

    nav clean up

---
+++ dist/scripts/script.js
@@ -0,0 +1,51 @@
+// Insert JavaScript here...
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("back-to-top").style.display = "block";
+  } else {
+    document.getElementById("back-to-top").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+function validateForm(){
+  $("form").on('submit',function(e){
+    e.preventDefault();
+  });
+
+  if(formIsValid()){    
+    document.getElementById("formCorrect").click();    
+  }
+  
+  if(!formIsValid()){
+    document.getElementById("formIncorrect").click();
+  }   
+}
+
+function formIsValid(){
+    var emailID = document.getElementById("inputEmail").value;
+    var fname = document.getElementById("inputFName").value;
+    var lname = document.getElementById("inputLName").value;
+    var msg = document.getElementById("inputMsg").value;
+    atpos = emailID.indexOf("@");
+    dotpos = emailID.lastIndexOf(".");
+
+    console.log(fname.length)
+    if(fname.length < 1 || lname.length < 1 || msg.length < 1){
+      return false;
+    }
+    
+    if (atpos < 1 || ( dotpos - atpos < 2 )) {
+       return false;
+    }
+
+    return true;
+}
