c004a50fe99069e618912134e165dc9ccb2f5d07
Trista Huang <trista.huang@outlook.com>
2019-04-12 14:17:20 -0700

    contact page added

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -15,3 +15,31 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 } 
+
+function validateForm(){
+  console.log("validate form")
+  if(formIsValid()){
+    document.getElementById("formCorrect").click();
+  }else{
+    document.getElementById("formIncorrect").click();
+  }   
+}
+
+function formIsValid(){
+    var emailID = document.getElementById("inputEmail").value;
+    var fname = document.getElementById("inputFName").value;
+    var lname = document.getElementById("inputLName").value;
+    var msg = document.getElementById("inputMsg").value;
+    atpos = emailID.indexOf("@");
+    dotpos = emailID.lastIndexOf(".");
+
+    console.log(fname.length)
+    if(fname.length < 1 || lname.length < 1 || msg.length < 1){
+      return false;
+    }
+    
+    if (atpos < 1 || ( dotpos - atpos < 2 )) {
+       return false;
+    }
+    return( true ); 
+}
